f79844372da4cca7a2ce25b94270828a210bed36
Javier Arias <apedro@unicauca.edu.co>
2025-07-31 20:07:09 -0500

    Migracion a output

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, input, Output } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { RouterLinkWithHref } from '@angular/router';
 import { Product } from '@shared/models/product.model';
@@ -15,7 +15,7 @@ export class ProductComponent {
 
   isPriority = input<boolean>(false);
 
-  @Output() addToCart = new EventEmitter();
+  addToCart = output<Product>();
 
   addToCartHandler() {
     this.addToCart.emit(this.product());
